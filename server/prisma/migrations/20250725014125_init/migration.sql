7350b33f370def79399c0f43b478be1f74b1435a
jeongeun.kim <jeongeun587@gmail.com>
2025-07-25 10:46:15 +0900

    reset prisma

--- server/prisma/migrations/20250725014125_init/migration.sql
+++ server/prisma/migrations/20250725014125_init/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `TestUser2` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "TestUser2";
-
 -- CreateTable
 CREATE TABLE "TestUser" (
     "id" TEXT NOT NULL,
